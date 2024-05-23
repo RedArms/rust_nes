8f95ddaa9f25fc569d0892ac0f9c076a6b878fac
RedArms <reda_1030@hotmail.com>
2024-05-23 15:58:39 +0200

    INC opcodes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::iter::FlatMap;
+
 fn main() {
 }
 
@@ -9,7 +11,22 @@ struct CPU {
     y:u8,
     flags:u8,
     stackpointer:u8,
+
 }
+
+#[repr(u8)]
+#[derive(Clone, Copy)]
+enum FLAGS {
+    CARRY     = 0,
+    ZERO      = 1,
+    INTERRUPT = 2,
+    DECIMAL   = 3,
+    B         = 4,
+    ONE       = 5,
+    OVERFLOW  = 6,
+    NEGATIVE  = 7
+}
+
 
 impl CPU {
 
@@ -29,11 +46,54 @@ impl CPU {
     fn fetch_next_three_byte(&self) -> (u8,u8,u8) {
         return (self.ram[self.pc as usize+ 1],self.ram[self.pc as usize+ 2],self.ram[self.pc as usize+ 3]);
     }
+    fn set_flag(&mut self,up:bool,flag:FLAGS) {
+        if up {
+            self.flags = self.flags | 1 << (flag as u8);
+        }
+        else {
+            self.flags = self.flags & (0xF ^ (1 << flag as u8));
+        }
+    }
+    fn get_xth_bit(x:u8,element:u8) -> bool {
+        return ((element>>x) & 0xFE) > 0
+    }
+
+    fn check_zero_carry(&mut self,x:u8) {
+        if x == 0 {
+            self.set_flag(true, FLAGS::ZERO)
+        }
+        if Self::get_xth_bit(7, x) {
+            self.set_flag(true, FLAGS::NEGATIVE)
+
+        }
+    }
 
     fn execute(&mut self) {
         let opcode = self.ram[self.pc as usize];
 
         match opcode {
+
+            //CLEAR FLAGS
+            //CLC
+            0x18=>{
+                self.set_flag(false, FLAGS::CARRY);
+                self.pc +=1;
+            },
+            //CLD
+            0xD8=>{
+                self.set_flag(false, FLAGS::DECIMAL);
+                self.pc +=1;
+            },
+            //CLI
+            0x58=>{
+                self.set_flag(false, FLAGS::INTERRUPT);
+                self.pc +=1;
+            },
+            //CLV
+            0xB8=>{
+                self.set_flag(false, FLAGS::OVERFLOW);
+                self.pc +=1;
+            }
 
             //JMP absolute
             0x4C=>{
@@ -42,6 +102,42 @@ impl CPU {
             //NOP
             0xEA=>{
                 self.pc +=1;
+            },
+            
+
+            //INC ZP
+            0xE6=>{
+                self.ram[self.fetch_next_byte() as usize] 
+                = self.ram[self.fetch_next_byte() as usize] + 1;
+                self.check_zero_carry(self.ram[self.fetch_next_byte() as usize]);
+            },
+            //INC ZPX
+            0xF6=>{
+                self.ram[(self.fetch_next_byte() + self.x) as usize] 
+                = self.ram[(self.fetch_next_byte() + self.x) as usize] + 1;
+                self.check_zero_carry(self.ram[(self.fetch_next_byte() + self.x) as usize]);
+            },
+            //INC ABS
+            0xEE=>{
+                self.ram[self.fetch_next_two_byte_as_u16() as usize]
+                 = self.ram[self.fetch_next_two_byte_as_u16() as usize] + 1;
+                self.check_zero_carry(self.ram[self.fetch_next_two_byte_as_u16() as usize]);
+            }, 
+            //INC ABS
+            0xFE=>{
+                self.ram[(self.fetch_next_two_byte_as_u16() + self.x as u16) as usize] 
+                = self.ram[(self.fetch_next_two_byte_as_u16() + self.x as u16) as usize] + 1;
+                self.check_zero_carry(self.ram[(self.fetch_next_two_byte_as_u16() + self.x as u16) as usize]);
+            },                       
+            //INX
+            0xE8=>{
+                self.x = self.x + 1;
+                self.check_zero_carry(self.x);
+            },
+            //INY
+            0xC8=>{
+                self.y = self.y + 1;
+                self.check_zero_carry(self.y);
             }
             _=>{print!("Unknow opcode : {:#04x}", opcode);}
         }
